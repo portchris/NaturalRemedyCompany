269eab1fa74fbf8286078caf79852e7aebb71187
Chris Rogers <chris@portchris.co.uk>
2019-01-31 14:21:45 +0000

    Set magento permissions and ownership

--- env/php72/scripts/start.sh
+++ env/php72/scripts/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Set Magento file permissions and ownership
+SRC=/home/www/naturalremedy/src
+chown -R www:www $SRC
+find $SRC -type f -exec chmod 400 {} +
+find $SRC -type d -exec chmod 500 {} +
+find $SRC/var/ -type f -exec chmod 600 {} +
+find $SRC/media/ -type f -exec chmod 600 {} +
+find $SRC/var/ -type d -exec chmod 700 {} +
+find $SRC/media/ -type d -exec chmod 700 {} +
+chmod 700 $SRC/includes
+chmod 600 $SRC/includes/config.php
+
+# Start CRON service
 service cron start
 
+# Start PHP service
 php-fpm7.2
