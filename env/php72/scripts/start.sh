ea3f7d25b472167f9cebd8f2a32f275bed115013
Chris Rogers <chris@portchris.co.uk>
2019-02-05 16:53:29 +0000

    Release such tight permissions

--- env/php72/scripts/start.sh
+++ env/php72/scripts/start.sh
@@ -3,8 +3,8 @@
 # Set Magento file permissions and ownership
 SRC=/home/www/naturalremedy/src
 chown -R www:www $SRC
-find $SRC -type f -exec chmod 400 {} +
-find $SRC -type d -exec chmod 500 {} +
+find $SRC -type f -exec chmod 600 {} +
+find $SRC -type d -exec chmod 600 {} +
 find $SRC/var/ -type f -exec chmod 600 {} +
 find $SRC/media/ -type f -exec chmod 600 {} +
 find $SRC/var/ -type d -exec chmod 700 {} +
