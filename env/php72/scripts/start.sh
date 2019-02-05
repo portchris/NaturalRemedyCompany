02492e31bf7f3b8e2e12e75ba7ff6a714e611558
Chris Rogers <chris@portchris.co.uk>
2019-02-05 16:59:49 +0000

    Release such tight permissions for directories

--- env/php72/scripts/start.sh
+++ env/php72/scripts/start.sh
@@ -4,7 +4,7 @@
 SRC=/home/www/naturalremedy/src
 chown -R www:www $SRC
 find $SRC -type f -exec chmod 600 {} +
-find $SRC -type d -exec chmod 600 {} +
+find $SRC -type d -exec chmod 700 {} +
 find $SRC/var/ -type f -exec chmod 600 {} +
 find $SRC/media/ -type f -exec chmod 600 {} +
 find $SRC/var/ -type d -exec chmod 700 {} +
