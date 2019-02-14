757c0602884611cd023f86593ab5b39b710e4ce9
Chris Rogers <chris@portchris.co.uk>
2019-02-14 16:01:47 +0000

    Add main blocks for Face and Figure site, remove TLSv1 from ssl params

---
+++ src/app/code/local/NaturalRemedyCo/FaceAndFigure/Block/Footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Main block class for Face & Figure Salon Onepage React Web App
+ * 
+ * @author      Chris Rogers
+ * @package     rwd_faceandfigure_default
+ * @since       2019-01-12
+*/
+class NaturalRemedyCo_FaceAndFigure_Block_Footer extends Mage_Core_Block_Template 
+{
+	public function __construct() 
+	{
+		
+	}
+}
+?>
