643b3005e1c236db63ba7599b32bceaf52472404
Chris Rogers <chris@portchris.co.uk>
2019-02-16 14:27:52 +0000

    Load Require.JS, fix config error, insert main Magento Blocks, start writing React Components

--- src/app/code/local/NaturalRemedyCo/FaceAndFigure/Block/Header.php
+++ src/app/code/local/NaturalRemedyCo/FaceAndFigure/Block/Header.php
@@ -8,9 +8,34 @@
 */
 class NaturalRemedyCo_FaceAndFigure_Block_Header extends Mage_Core_Block_Template 
 {
+	/**
+	 * @var string
+	 */
+	const REACT_COMPONENT = "header";
+
+	/**
+	 * @var Mage_Core_Model_Store
+	 */
+	protected $_store;
+
 	public function __construct() 
 	{
-		
+		$this->_store = Mage::app()->getStore();
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getBlockConfig()
+	{
+		return json_encode([
+			"title" => $this->_store->getFrontendName() 
+		], true);
+	}
+
+	public function getReactComponent()
+	{
+		return Mage::getSingleton('core/design_package')->getSkinBaseDir() . DS . "js" . DS . "components" . DS . self::REACT_COMPONENT . ".js";
 	}
 }
 ?>
