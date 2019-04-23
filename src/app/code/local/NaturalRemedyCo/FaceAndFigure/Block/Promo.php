f5992ebb98baf2755ba437eafa9152f76e9dc857
Chris Rogers <chris@portchris.co.uk>
2019-04-23 16:08:08 +0100

    Add promo and hero to homepage

--- src/app/code/local/NaturalRemedyCo/FaceAndFigure/Block/Promo.php
+++ src/app/code/local/NaturalRemedyCo/FaceAndFigure/Block/Promo.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Hero block class for Face & Figure Salon Onepage React Web App
+ * Promo block class for Face & Figure Salon Onepage React Web App
  * 
  * @author      Chris Rogers
  * @package     rwd_faceandfigure_default
  * @since       2019-01-12
 */
-class NaturalRemedyCo_FaceAndFigure_Block_Hero extends NaturalRemedyCo_FaceAndFigure_Block_PageAbstract implements NaturalRemedyCo_FaceAndFigure_Block_PageInterface
+class NaturalRemedyCo_FaceAndFigure_Block_Promo extends NaturalRemedyCo_FaceAndFigure_Block_PageAbstract implements NaturalRemedyCo_FaceAndFigure_Block_PageInterface
 {
-	const REACT_COMPONENT = "hero";
+	const REACT_COMPONENT = "promo";
 
 	public function __construct() 
 	{
@@ -21,7 +21,7 @@ class NaturalRemedyCo_FaceAndFigure_Block_Hero extends NaturalRemedyCo_FaceAndFi
 	public function getBlockConfig()
 	{
 		return [
-			"data" => $this->getLayout()->createBlock('cms/block')->setBlockId('faceandfigure_hero')->toHtml()
+			"data" => $this->getLayout()->createBlock('cms/block')->setBlockId('faceandfigure_selling_points_hero_breakdown')->toHtml()
 		];
 	}
 
