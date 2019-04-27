7eabc274b17a87040e9f8dc5ead59481fde1e148
Chris Rogers <chris@portchris.co.uk>
2019-04-27 13:24:41 +0100

    Update NPM, theme changes

--- npm.sh
+++ npm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose exec -u root nr_php72 npm --prefix /home/www/naturalremedy/src/app/design/frontend/rwd_faceandfigure/default/faceandfiguresalon/ $@
+docker-compose exec -u www nr_php72 npm --prefix /home/www/naturalremedy/src/app/design/frontend/rwd_faceandfigure/default/faceandfiguresalon/ $@
