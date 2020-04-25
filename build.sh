83b73641811f4590708ad9de4f8a58910b9c1207
Chris Rogers <chris@portchris.co.uk>
2020-04-25 11:40:15 +1000

    Build script with no interaction

--- build.sh
+++ build.sh
@@ -14,5 +14,19 @@ if [ -z ${GROUP_ID+x} ]; then
 	GROUP_ID=$(id -g)
 fi
 
+NO_CACHE="--no-cache"
+if [[ $* == *-n* ]]; then
+	NO_CACHE="--pull"
+fi
 
-docker-compose build --build-arg UID=$USER_ID --build-arg GID=$GROUP_ID
+if [[ $NO_CACHE == "--no-cache" ]]; then
+	read -p "Use Docker cache? [Y/y]? " -n 1 -r
+	echo # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]; then 
+		NO_CACHE="--pull"
+	else 
+		NO_CACHE="--no-cache"
+	fi
+fi
+
+docker-compose build $NO_CACHE --build-arg UID=$USER_ID --build-arg GID=$GROUP_ID
